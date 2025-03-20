ec19881f273f72f17b9e63140640baac18240314
ge53tn <nilsu.tuna@tum.de>
2025-03-20 12:09:46 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 
-blockMesh // > log.blockMesh 2>&1
+blockMesh > log.blockMesh 2>&1
 
-surfaceFeatureExtract // > log.surfaceFeatureExtract 2>&1
+surfaceFeatureExtract > log.surfaceFeatureExtract 2>&1
 
-snappyHexMesh -overwrite // > log.snappyHexMesh 2>&1
+snappyHexMesh -overwrite > log.snappyHexMesh 2>&1
 
 setFields
 
-//rm log.*
+rm log.*
 
-decomposePar // > log.decomposePar 2>&1
+decomposePar > log.decomposePar 2>&1
 
-mpirun -np 8 interFoam -parallel // > log.simpleFoam 2>&1
+mpirun -np 8 interFoam -parallel > log.simpleFoam 2>&1
 
-reconstructPar // > log.reconstructPar 2>&1
+reconstructPar > log.reconstructPar 2>&1
 
-rm -rf proc* //dene
-
+rm -rf proc* 
